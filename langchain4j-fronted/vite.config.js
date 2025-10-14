b6410be36627e4757a7821a70a67de965a7ea129
huliua <1148247580@qq.com>
2025-10-14 22:03:29 +0800

    后端做跨域处理；修改系统提示词

--- langchain4j-fronted/vite.config.js
+++ langchain4j-fronted/vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
         proxy: {
             '/api': {
                 target: 'http://localhost:8080',
-                changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
--- src/main/java/com/huliua/langChain4j/controller/ChatController.java
+++ src/main/java/com/huliua/langChain4j/controller/ChatController.java
@@ -2,7 +2,6 @@ package com.huliua.langChain4j.controller;
 
 import com.huliua.langChain4j.aiservice.AiChatService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,7 @@ public class ChatController {
         return aiChatService.chat(message);
     }
 
-    @PostMapping(value = "/streamChat", produces = "text/html;charset=utf8")
+    @RequestMapping(value = "/streamChat")
     public Flux<String> streamChat(String memoryId, String message) {
         return aiChatService.chatStream(memoryId, message);
     }
